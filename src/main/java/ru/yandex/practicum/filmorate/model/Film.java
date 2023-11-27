cacaba0e2c01e2e9773c06c58074f5ecec9d8493
AndrejSinyavin <140056510+AndrejSinyavin@users.noreply.github.com>
2023-11-27 15:51:59 +0700

    Delete .mvn directory

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,7 +6,6 @@ import org.springframework.validation.annotation.Validated;
 import service.StartYear;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
