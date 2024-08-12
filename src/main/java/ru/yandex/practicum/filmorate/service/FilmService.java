b1b5031500bcf15d69b76a6b916e72ce91f1c595
Alexander Martynsh <avmart31@mts.ru>
2024-08-12 16:58:19 +0300

    правка замечаний

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -169,16 +169,17 @@ public class FilmService implements BaseFilmService {
         return films.getCommonFilms(userId, friendId);
     }
 
+    @Override
+    public void deleteFilm(int id) {
+        films.deleteFilmById(id);
+    }
+
     /**
      * Метод проверяет, что ID MPA-рейтинга, ID в списках жанров и режиссеров имеются в БД,
      * и присваивает соответствующие названия полям фильма по этим ID. В списках удаляются повторы.
      *
      * @param film фильм, в котором нужно проверить ID и присвоить полям названия
      */
-    @Override
-    public void deleteFilm(int id) {
-        films.deleteFilmById(id);
-    }
 
     private void validateAndUpdateFilm(Film film) {
         film.setMpa(getMpa(film));
