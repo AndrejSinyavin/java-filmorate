4ee218891a86131ed526a2c8914956175042128e
Alexander Martynsh <avmart31@mts.ru>
2024-08-13 09:02:26 +0300

    правка стиля

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -13,7 +13,6 @@ import ru.yandex.practicum.filmorate.service.BaseFilmService;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.List;
-import java.util.Set;
 
 /**
  * Контроллер обработки REST-запросов для работы с фильмотекой.
