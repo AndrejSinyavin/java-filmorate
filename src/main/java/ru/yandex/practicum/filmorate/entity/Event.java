be159ed64ebf400c1661fa1ebd33014428cc6eda
ArturGizzatullin02 <salatikufa@yandex.ru>
2024-08-15 16:53:17 +0500

    исправление по ревью

--- src/main/java/ru/yandex/practicum/filmorate/entity/Event.java
+++ src/main/java/ru/yandex/practicum/filmorate/entity/Event.java
@@ -3,29 +3,32 @@ package ru.yandex.practicum.filmorate.entity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Event {
     @NotNull(message = "ID события не может быть null")
     @Positive(message = "ID события не может быть отрицательным значением")
     private int eventId;
     @NotNull(message = "Дата события не может быть null")
-    private long timestamp;
+    long timestamp;
     @NotNull(message = "ID пользователя не может быть null")
     @Positive(message = "ID пользователя не может быть отрицательным значением")
-    private int userId;
-    @NotBlank(message = "Тип события не может быть null")
-    private String eventType;
-    @NotBlank(message = "Операция события не может быть null")
-    private String operation;
+    int userId;
+    @NotBlank(message = "Тип события не может быть пустым")
+    String eventType;
+    @NotBlank(message = "Операция события не может быть пустым")
+    String operation;
     @NotNull(message = "ID сущности не может быть null")
     @Positive(message = "ID сущности не может быть отрицательным значением")
-    private int entityId;
+    int entityId;
 
     public Event(long instant, int userId, String eventType, String operation, int entityId) {
         this.timestamp = instant;
