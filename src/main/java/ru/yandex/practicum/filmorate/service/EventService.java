67d422672df6be7ba5284436b711bc73a28f796f
Elen-B <beskrovnaia.elena@yandex.ru>
2024-08-15 08:25:08 +0500

    add check for user exist

--- src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.entity.Event;
+import ru.yandex.practicum.filmorate.exception.EntityNotFoundException;
 import ru.yandex.practicum.filmorate.repository.EventRepository;
+import ru.yandex.practicum.filmorate.repository.UserRepository;
 
 import java.util.Collection;
 
@@ -13,9 +15,15 @@ import java.util.Collection;
 @Slf4j
 public class EventService implements BaseEventService {
     private final EventRepository eventRepository;
+    private final UserRepository userRepository;
 
     @Override
     public Collection<Event> getFeed(int userId) {
+        if (userRepository.getUser(userId).isEmpty()) {
+            throw new EntityNotFoundException(getClass().getSimpleName(),
+                    EntityNotFoundException.class.getName(),
+                    "Пользователь с указанным id не найден");
+        }
         return eventRepository.getAllFriendsEventsByUserId(userId);
     }
 }
